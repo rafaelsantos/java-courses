61a328fea808b14d3a2abac0c2fc83654a2e9fdf
rafaelsantos <santos.rafaelbs@gmail.com>
2019-12-04 12:32:48 -0300

    Creating project structure for Spring REST Basic Course from Algaworks

---
+++ algaworks/spring-rest-basic-course/bookstore-api/src/main/java/com/github/rafaelsantos/bookstore/BookstoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.rafaelsantos.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApiApplication.class, args);
+	}
+
+}
